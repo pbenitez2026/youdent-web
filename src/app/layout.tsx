4adcd109462697dcb71eda06ff7951b065613fd9
Mauricio Naal <pbenitez@youdent.com.mx>
2026-05-21 17:05:42 -0600

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-X8S6R8BDXB"></script>
+
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-X8S6R8BDXB');
+      `,
+    }}
+  />
+</head>
       <body>{children}</body>
     </html>
   );
