f5192cec18be7e501d1465f8ef7020175b73f6c8
Mauricio Naal <pbenitez@youdent.com.mx>
2026-05-22 21:34:06 -0600

    fix google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Clínica YouDent",
-  description: "Clínica dental en CDMX con atención cercana, diagnóstico claro y tratamientos personalizados en estética dental, implantes, ortodoncia y endodoncia.",
+  description:
+    "Clínica dental en CDMX con atención cercana, diagnóstico claro y tratamientos personalizados en estética dental, implantes, ortodoncia y endodoncia.",
   icons: {
     icon: "/favicon.png",
     shortcut: "/favicon.png",
@@ -18,19 +20,21 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-X8S6R8BDXB"></script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X8S6R8BDXB"
+          strategy="afterInteractive"
+        />
 
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-X8S6R8BDXB');
-      `,
-    }}
-  />
-</head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X8S6R8BDXB');
+          `}
+        </Script>
+      </head>
+
       <body>{children}</body>
     </html>
   );
